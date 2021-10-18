aad1eeb4b645eec26d7aeadc984a78082b5ac2cf
WaylandM <mw283@cam.ac.uk>
2021-10-18 14:42:33 +0100

    renamed:    wio-terminal-lcd/example/patch.xodp -> wio-terminal-lcd/01-example/patch.xodp 	modified:   wio-terminal-lcd/draw-rectangle/patch.xodp 	new file:   wio-terminal-lcd/draw-rounded-rectangle/patch.cpp 	new file:   wio-terminal-lcd/draw-rounded-rectangle/patch.xodp

---
+++ wio-terminal-lcd/draw-rounded-rectangle/patch.cpp
@@ -0,0 +1,32 @@
+
+node {
+    void evaluate(Context ctx) {
+        // The node responds only if there is an input pulse
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
+
+        // Get a pointer to the `TFT_eSPI tft` class instance
+        auto tft = getValue<input_DEV>(ctx);
+
+        XColor color24 = getValue<input_Color>(ctx);
+        uint16_t color16 = ((color24.r & 0xF8) << 8) | ((color24.g & 0xFC) << 3) | (color24.b >> 3);
+
+        if (getValue<input_Fill>(ctx)) {
+            tft -> fillRect(getValue<input_X>(ctx),
+                            getValue<input_Y>(ctx),
+                            getValue<input_W>(ctx),
+                            getValue<input_H>(ctx),
+                            getValue<input_R>(ctx),
+                            color16);
+        } else {
+            tft -> drawRect(getValue<input_X>(ctx),
+                            getValue<input_Y>(ctx),
+                            getValue<input_W>(ctx),
+                            getValue<input_H>(ctx),
+                            getValue<input_R>(ctx),
+                            color16);
+        }
+
+        emitValue<output_Done>(ctx, 1);
+    }
+}
