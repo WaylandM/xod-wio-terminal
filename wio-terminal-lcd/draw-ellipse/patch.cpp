0c7190651e6ae06ae29fa6ad68838724c143ee5b
WaylandM <mw283@cam.ac.uk>
2021-10-22 22:41:47 +0100

    new file:   wio-terminal-lcd/draw-ellipse/patch.cpp 	new file:   wio-terminal-lcd/draw-ellipse/patch.xodp 	modified:   wio-terminal-lcd/draw-rounded-rectangle/patch.cpp

---
+++ wio-terminal-lcd/draw-ellipse/patch.cpp
@@ -0,0 +1,22 @@
+
+node {
+    void evaluate(Context ctx) {
+        // The node responds only if there is an input pulse
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
+
+        // Get a pointer to the `TFT_eSPI tft` class instance
+        auto tft = getValue<input_DEV>(ctx);
+
+        XColor color24 = getValue<input_Color>(ctx);
+        uint16_t color16 = ((color24.r & 0xF8) << 8) | ((color24.g & 0xFC) << 3) | (color24.b >> 3);
+
+        if (getValue<input_Fill>(ctx)) {
+            tft -> fillEllipse(getValue<input_X>(ctx),  getValue<input_Y>(ctx), getValue<input_RX>(ctx), getValue<input_RY>(ctx), color16);
+        } else {
+            tft -> drawEllipse(getValue<input_X>(ctx),  getValue<input_Y>(ctx), getValue<input_RX>(ctx), getValue<input_RY>(ctx), color16);
+        }
+
+        emitValue<output_Done>(ctx, 1);
+    }
+}
--- wio-terminal-lcd/draw-rounded-rectangle/patch.cpp
+++ wio-terminal-lcd/draw-rounded-rectangle/patch.cpp
@@ -12,14 +12,14 @@ node {
         uint16_t color16 = ((color24.r & 0xF8) << 8) | ((color24.g & 0xFC) << 3) | (color24.b >> 3);
 
         if (getValue<input_Fill>(ctx)) {
-            tft -> fillRect(getValue<input_X>(ctx),
+            tft -> fillRoundRect(getValue<input_X>(ctx),
                             getValue<input_Y>(ctx),
                             getValue<input_W>(ctx),
                             getValue<input_H>(ctx),
                             getValue<input_R>(ctx),
                             color16);
         } else {
-            tft -> drawRect(getValue<input_X>(ctx),
+            tft -> drawRoundRect(getValue<input_X>(ctx),
                             getValue<input_Y>(ctx),
                             getValue<input_W>(ctx),
                             getValue<input_H>(ctx),
