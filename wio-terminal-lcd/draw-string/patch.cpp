d27c60708199962ae556798fe3ce00e647267430
WaylandM <mw283@cam.ac.uk>
2021-10-24 18:24:06 +0100

    modified:   wio-terminal-lcd/draw-character/patch.xodp 	new file:   wio-terminal-lcd/draw-string/patch.cpp 	new file:   wio-terminal-lcd/draw-string/patch.xodp 	new file:   wio-terminal-lcd/set-rotation/patch.cpp 	modified:   wio-terminal-lcd/set-rotation/patch.xodp

---
+++ wio-terminal-lcd/draw-string/patch.cpp
@@ -0,0 +1,29 @@
+
+node {
+    void evaluate(Context ctx) {
+        // The node responds only if there is an input pulse
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
+
+        // Get a pointer to the `TFT_eSPI tft` class instance
+        auto tft = getValue<input_DEV>(ctx);
+
+        auto xString = getValue<input_String>(ctx);
+        int N=length(xString) + 1;
+        char cString[N];
+        for(int i=0;i<N;i++)
+            cString[i]=0;
+        dump(xString, cString);
+
+        XColor color24 = getValue<input_Color>(ctx);
+        uint16_t color16 = ((color24.r & 0xF8) << 8) | ((color24.g & 0xFC) << 3) | (color24.b >> 3);
+
+        tft -> setTextColor(color16);
+        tft -> setTextSize(getValue<input_Size>(ctx));
+        tft -> drawChar(getValue<input_X>(ctx),
+                        getValue<input_Y>(ctx),
+                        cString);
+
+        emitValue<output_Done>(ctx, 1);
+    }
+}
---
+++ wio-terminal-lcd/set-rotation/patch.cpp
@@ -0,0 +1,15 @@
+
+node {
+    void evaluate(Context ctx) {
+        // The node responds only if there is an input pulse
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
+
+        // Get a pointer to the `TFT_eSPI tft` class instance
+        auto tft = getValue<input_DEV>(ctx);
+
+        tft -> setRotation(getValue<input_Rotate>(ctx));
+
+        emitValue<output_Done>(ctx, 1);
+    }
+}
