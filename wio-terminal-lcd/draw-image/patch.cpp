03cca0784b964688a165572ed071a9fb08c96733
WaylandM <mw283@cam.ac.uk>
2021-11-19 22:43:02 +0000

    modified:   wio-terminal-lcd/05-example/patch.xodp 	modified:   wio-terminal-lcd/draw-image/patch.cpp 	modified:   wio-terminal-lcd/draw-image/patch.xodp 	modified:   wio-terminal-lcd/test/patch.xodp

--- wio-terminal-lcd/draw-image/patch.cpp
+++ wio-terminal-lcd/draw-image/patch.cpp
@@ -7,14 +7,10 @@
 #include<SD/Seeed_SD.h>
 
 node {
-    //meta {
-        // Define our custom type as a pointer on the class instance.
-        //using Type = TFT_eSPI*;
-    //}
 
     void evaluate(Context ctx) {
         if(isSettingUp()){
-            delay(2000);
+            delay(3000);
             //Initialise SD card
             if (!SD.begin(SDCARD_SS_PIN, SDCARD_SPI)) {
                 raiseError(ctx);
@@ -38,37 +34,29 @@ node {
 
         File f = SD.open(cString, FILE_READ);
         if (!f){
+            raiseError(ctx);
             return;
         }
         int32_t size = f.size()-4;
 
         uint8_t header [4];
         f.read(header, 4);
-/*
-        if(getValue<input_16_bit>(ctx)) {
-            //uint16_t mem [size/256];
-            //drawImage<uint16_t>(cString, getValue<input_X>(ctx), getValue<input_Y>(ctx));
-        } else {
-            //uint8_t mem [size];
-            //drawImage<uint8_t>(cString, getValue<input_X>(ctx), getValue<input_Y>(ctx));
-        }
-        */
-        //uint8_t mem [size];
-        //f.read(mem, size);
-        f.close();
-
-
-        //tft->setSwapBytes(false);
-        //tft->pushImage(0,0,320,238,mem);
-
         int16_t w = ((header[1] << 8) | header[0]);
         int16_t h = ((header[3] << 8) | header[2]);
-        
-        emitValue<output_N>(ctx, size);
-        
-        emitValue<output_W>(ctx, w);
-        emitValue<output_H>(ctx, h);
-        
+
+        if(getValue<input_16_bit>(ctx)) {
+            uint16_t *mem = new uint16_t[size/2];
+            f.read(mem, size);
+            tft->pushImage(getValue<input_X>(ctx), getValue<input_Y>(ctx), w, h, mem);
+            delete [] mem;
+        } else {
+            uint8_t *mem = new uint8_t[size];
+            f.read(mem, size);
+            tft->pushImage(getValue<input_X>(ctx), getValue<input_Y>(ctx), w, h, mem);
+            delete [] mem;
+        }
+
+        f.close();        
         
         emitValue<output_Done>(ctx, 1);
     }
