d66b51c83bfaf6955af6adda7c17e652f14e3912
WaylandM <mw283@cam.ac.uk>
2021-12-17 22:53:02 +0000

    new file:   wio-terminal-lcd-sprite/draw-triangle/patch.cpp 	modified:   wio-terminal-lcd-sprite/draw-triangle/patch.xodp 	new file:   wio-terminal-lcd-sprite/draw-vertical-line/patch.cpp 	modified:   wio-terminal-lcd-sprite/draw-vertical-line/patch.xodp 	new file:   wio-terminal-lcd-sprite/initialize-sprite/patch.cpp 	modified:   wio-terminal-lcd-sprite/initialize-sprite/patch.xodp

---
+++ wio-terminal-lcd-sprite/draw-vertical-line/patch.cpp
@@ -0,0 +1,18 @@
+
+node {
+    void evaluate(Context ctx) {
+        // The node responds only if there is an input pulse
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
+
+        // Get a pointer to the `TFT_eSprite sprite` class instance
+        auto spr = getValue<input_Sprite>(ctx);
+
+        XColor color24 = getValue<input_Color>(ctx);
+        uint16_t color16 = ((color24.r & 0xF8) << 8) | ((color24.g & 0xFC) << 3) | (color24.b >> 3);
+
+        spr -> drawFastVLine(getValue<input_X>(ctx),  getValue<input_Y>(ctx), getValue<input_H>(ctx), color16);
+
+        emitValue<output_Done>(ctx, 1);
+    }
+}
